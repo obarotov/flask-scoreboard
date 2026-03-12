5766c0c03ec35caf4f58e61d8d86ba34a223b65e
otabek <otabekbarotov15@gmail.com>
2026-03-12 13:02:30 +0500

    JSON API method was added

--- app.py
+++ app.py
@@ -1,40 +1,8 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,jsonify
 
 app = Flask(__name__)
 
-
-@app.route("/")
-def main():
-    return render_template("/main_page.html")
-
-@app.route("/about")
-def about():
-    return render_template("/about.html")
-
-
-
-@app.route('/students')
-def students():
- students_data = {
-    "Abdul-Aziz": 77, "Abdullo": 47, "Abubakr": 59,
-    "Ahmadjon": 69, "Amir": 44, "Behruz": 69,
-    "Habibulloh": 51, "Ilyos": 64, "Maftunbek": 39,
-    "Maryam": 28, "Mubarro": 22, "Najmiya": 60,
-    "Otabek": 48, "Shahrom": 83, "Habibullo": 63,
-    "Rukhshona": 31, "Osiya": 69, "Bakhtiyor": 32,
-    "Muhammadjon": 0, "Timur": 89, "Gulsum": 36,
-    "Rayhona": 39, "Abduvozit": 12, "Ismoiljon": 0,
-    "Nizar": 72,
-}
- sorted_students = sorted(students_data.items(), key=lambda x: x[1], reverse=True)
-
- return render_template('students.html', students=sorted_students)
-
-@app.route("/student/<name>")
-def student_detail(name):
-    name  = name.title() 
-
-    students_data = {
+students_data = {
         "Abdul-Aziz": 77, "Abdullo": 47, "Abubakr": 59,
         "Ahmadjon": 69, "Amir": 44, "Behruz": 69,
         "Habibulloh": 51, "Ilyos": 64, "Maftunbek": 39,
@@ -44,7 +12,29 @@ def student_detail(name):
         "Muhammadjon": 0, "Timur": 89, "Gulsum": 36,
         "Rayhona": 39, "Abduvozit": 12, "Ismoiljon": 0,
         "Nizar": 72,
-    }
+}
+
+
+
+@app.route("/")
+def main():
+    return render_template("/main_page.html")
+
+
+@app.route("/about")
+def about():
+    return render_template("/about.html")
+
+
+@app.route('/students')
+def students():
+ sorted_students = sorted(students_data.items(), key=lambda x: x[1], reverse=True)
+
+ return render_template('students.html', students=sorted_students)
+
+@app.route("/student/<name>")
+def student_detail(name):
+    name  = name.title() 
 
     if name not in students_data:
         return render_template("/404.html")
@@ -61,6 +51,45 @@ def student_detail(name):
         result = "No data"
     
     return render_template("student.html", name=name, score=score, result=result)
+
+
+@app.route("/api/students")
+def all_students():
+    return jsonify(students_data)
+
+@app.route("/api/student/<name>")
+def one_student(name):
+    name = name.title()
+    if name not in students_data:
+        return jsonify({"error": "Student not found"}), 404
+   
+    score = students_data[name]
+    if score >= 80:
+        feedback = 'Excellent'
+    elif 50 <= score <= 79:
+        print("Good")
+    elif 1 <= score <= 49:
+        feedback = "Needs improvement"
+    else:
+        feedback = "No data"
+
+    fresult = {
+        "name": name,
+        "score": score,
+        "status": feedback
+
+    }
+    return jsonify(fresult)
+        
+        
+
+
+@app.route("/add")
+def add(name):
+    if name not in students_data:
+        return f"Success"
+    
+
 
 
 if __name__ == "__main__":
